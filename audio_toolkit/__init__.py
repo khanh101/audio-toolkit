500109e0a2c8a0569a06b7632aa5c72c27728b28
khanh nguyen <khanh.nguyen.contact@gmail.com>
2023-07-27 16:40:15 +0800

    auto_push

--- audio_toolkit/__init__.py
+++ audio_toolkit/__init__.py
@@ -1,1 +1,2 @@
 from .audio_stats import AudioStats
+from .audio_converter import convert_to_wav_mono_16k
---
+++ audio_toolkit_example/audio_normalizer_example.py
@@ -0,0 +1,4 @@
+from audio_toolkit import convert_to_wav_mono_16k
+
+if __name__ == "__main__":
+    convert_to_wav_mono_16k("test.wav", "out.wav")
