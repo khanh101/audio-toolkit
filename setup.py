7ac9b7244d35de6071c92495b1d2d7eeb91bdb75
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-04-17 16:54:39 +0800

    auto_commit

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ import setuptools
 if __name__ == "__main__":
     with open("README.md") as f:
         long_description = f.read()
+    
+    with open("version") as f:
+        version = int(f.read())
+
+    new_version = version+1
+    with open("version", "w") as f:
+        f.write(str(new_version))
 
     setuptools.setup(
         name="audio-toolkit",
-        version="0.5.0",
+        version=f"0.{version}.0",
         author="Nguyen Ngoc Khanh",
         author_email="khanh.nguyen.contact@gmail.com",
         long_description=long_description,
