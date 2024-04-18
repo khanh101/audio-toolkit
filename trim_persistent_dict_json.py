c8e8678b48d9d21298a2a85d3b5b3d0e1ee3aa77
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-04-18 14:26:10 +0800

    auto_commit

--- trim_persistent_dict_json.py
+++ trim_persistent_dict_json.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     cache_path = sys.argv[1]
     
     cache = {}
-    cache_str = open(self.cache_path).read().rstrip(",\n")
+    cache_str = open(cache_path).read().rstrip(",\n")
     o_list = json.loads(f"[{cache_str}]")
     for o in o_list:
         cache[o["key"]] = o["val"]
