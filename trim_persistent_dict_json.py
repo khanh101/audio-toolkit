eb845b5b61cd6bc028e1886fca805b5a92e317a5
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-04-18 14:27:42 +0800

    auto_commit

--- trim_persistent_dict_json.py
+++ trim_persistent_dict_json.py
@@ -2,19 +2,23 @@
 import json
 import os
 import sys
-
+import time
 from tqdm import tqdm
 
 if __name__ == "__main__":
     cache_path = sys.argv[1]
     
     cache = {}
+
+    t0 = time.time()
     cache_str = open(cache_path).read().rstrip(",\n")
     o_list = json.loads(f"[{cache_str}]")
     for o in o_list:
         cache[o["key"]] = o["val"]
-    
-    for k, v in cache.items():
+    t1 = time.time()
+    print(f"cache load time {cache_path}: {t1-t0}", file=sys.stderr)
+
+    for k, v in tqdm(cache.items(), desc="writing cache ..."):
         sys.stdout.write(json.dumps({
             "key": k,
             "val": v,
