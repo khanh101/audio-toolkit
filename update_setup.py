5b3a45f28ee1ea2a20edd245bfef54d9a1675552
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-04-18 11:32:03 +0800

    auto_commit

--- update_setup.py
+++ update_setup.py
@@ -1,11 +1,27 @@
-if __name__ == "__main__":
-    version = int(open("version").read())
-    new_version = version+1
-    open("version", "w").write(str(new_version))
 
-    version_str = f"version=\"0.{version}.0\""
-    new_version_str = f"version=\"0.{new_version}.0\""
+version = int(open("version").read())
+version +=1
+open("version", "w").write(str(version))
 
-    setup_str = open("setup.py").read()
-    new_setup_str = setup_str.replace(version_str, new_version_str)
-    open("setup.py", "w").write(new_setup_str)
+setup_str = f"""
+import setuptools
+
+setuptools.setup(
+    name="audio-toolkit",
+    version="0.{version}.0",
+    author="Nguyen Ngoc Khanh",
+    author_email="khanh.nguyen.contact@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/khanh101/audio-tools",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    install_requires=[
+        "tqdm==4.65.0",
+        "sqlitedict==2.1.0",
+    ],
+)
+"""
+
+
+open("setup.py", "w").write(setup_str)
