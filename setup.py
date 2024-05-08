311b49d1881a87539f3a599eb947546d27290481
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-05-08 11:58:33 +0800

    auto_commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="audio-toolkit",
-    version="0.44.0",
+    version="0.46.0",
     author="Nguyen Ngoc Khanh",
     author_email="khanh.nguyen.contact@gmail.com",
     long_description=open("README.md").read(),
